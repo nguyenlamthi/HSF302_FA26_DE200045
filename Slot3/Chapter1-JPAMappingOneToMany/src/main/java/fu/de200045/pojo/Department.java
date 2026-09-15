c114a24ef9041bb1c95955534442ab56ed680726
NguyenLamThi <nguyenlamthi2006@gmail.com>
2026-09-15 16:13:30 +0700

    feat(TODO 2.1): add entity Department, Employee, Gender

---
+++ Slot3/Chapter1-JPAMappingOneToMany/src/main/java/fu/de200045/pojo/Department.java
@@ -0,0 +1,60 @@
+package fu.de200045.pojo;
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "departments")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", unique = true)
+    private String name;
+
+    private String location;
+
+    public Department() {
+    }
+
+    public Department(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    // ---- Getters / Setters ----
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Department)) return false;
+        Department that = (Department) o;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Department{id=" + id + ", name='" + name + "'}";
+    }
+}
+
