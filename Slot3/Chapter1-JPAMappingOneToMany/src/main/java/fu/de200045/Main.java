43e9c5b207f12ddd1840ec25cd49af87d7bc0f42
NguyenLamThi <nguyenlamthi2006@gmail.com>
2026-09-15 20:23:10 +0700

    feat(TODO 2.7): Demo in Main

--- Slot3/Chapter1-JPAMappingOneToMany/src/main/java/fu/de200045/Main.java
+++ Slot3/Chapter1-JPAMappingOneToMany/src/main/java/fu/de200045/Main.java
@@ -4,6 +4,7 @@ import fu.de200045.dao.DepartmentDAO;
 import fu.de200045.pojo.Department;
 import fu.de200045.pojo.Employee;
 import fu.de200045.pojo.Gender;
+import fu.de200045.util.JPAUtil;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -11,9 +12,31 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        DepartmentDAO deptDAO = new DepartmentDAO();
-        Department dept = deptDAO.findByIdWithEmployees(2L);
-        List<Employee> employees = dept.getEmployees();
-        employees.forEach(System.out::println);
+        DepartmentDAO departmentDAO = new DepartmentDAO();
+
+        Department it = new Department("Marketing", "Ha Noi");
+
+        Employee e1 = new Employee("A", "a@company.com",
+                new BigDecimal("15000000"), Gender.MALE, LocalDate.of(2022, 1, 10));
+        Employee e2 = new Employee("B", "b@company.com",
+                new BigDecimal("20000000"), Gender.FEMALE, LocalDate.of(2022, 1, 15));
+        Employee e3 = new Employee("C", "c@company.com",
+                new BigDecimal("25000000"), Gender.OTHER, LocalDate.of(2022, 1, 20));
+
+        it.addEmployee(e1);
+        it.addEmployee(e2);
+        it.addEmployee(e3);
+
+        departmentDAO.saveDepartment(it);
+
+        System.out.println("Da luu Department, id = " + it.getId());
+
+        Department found = departmentDAO.findByIdWithEmployees(it.getId());
+        System.out.println("Phong ban: " + found.getName());
+        for (Employee e : found.getEmployees()) {
+            System.out.println("  - " + e);
+        }
+
+        JPAUtil.close();
     }
 }
